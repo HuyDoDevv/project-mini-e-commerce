e67dcb4d567575e1a8149155dc82dcfca6be3fd8
huydodev <doquanghuy2711@gmail.com>
2025-12-17 23:48:01 +0700

    write method user and add validation,middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,30 +1,15 @@
 package main
 
 import (
+	"project-mini-e-commerce/internal/app"
 	"project-mini-e-commerce/internal/config"
-	"project-mini-e-commerce/internal/handler"
-	"project-mini-e-commerce/internal/repository"
-	"project-mini-e-commerce/internal/routes"
-	"project-mini-e-commerce/internal/service"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config := config.NewConfig()
+	application := app.NewApplication(config)
 
-	userRepo := repository.NewMemoryUserRepository()
-
-	userSer := service.NewUserService(userRepo)
-
-	userHand := handler.NewUserHandler(userSer)
-
-	userRoutes := routes.NewUserRoutes(userHand)
-
-	r := gin.Default()
-
-	routes.RegisterRoutes(r, userRoutes)
-	if err := r.Run(config.ServerAddress); err != nil {
-		panic("Cannot run server")
+	if err := application.Run(); err != nil {
+		panic(err)
 	}
 }
